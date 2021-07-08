6757ae12412306356485b7c5c3881acce163614a
Wellvergton <wmc.jp@outlook.com>
2021-07-08 02:40:29 -0300

    First commit

---
+++ src/test/java/io/github/wellvergton/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wellvergton.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
